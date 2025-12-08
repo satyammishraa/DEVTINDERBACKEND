50b622d849a61412d8f5992cbdf5ef10e3458b9b
satyammishraa <163456205+satyammishraa@users.noreply.github.com>
2025-12-08 11:17:04 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,45 +4,50 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const http = require("http");
-
 require("dotenv").config();
-
 require("./utils/cronjob");
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://devtinderfrontendnew1.vercel.app",
+];
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 
+// Routers
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
-//const paymentRouter = require("./routes/payment");
-const initializeSocket = require("./utils/socket");
 const chatRouter = require("./routes/chat");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
-//app.use("/", paymentRouter);
 app.use("/", chatRouter);
 
 const server = http.createServer(app);
+
+// SOCKET FIX
+const initializeSocket = require("./utils/socket");
 initializeSocket(server);
 
 connectDB()
   .then(() => {
-    console.log("Database connection established...");
-    server.listen(process.env.PORT, () => {
-      console.log("Server is successfully listening on port 7777...");
+    console.log("Database connected");
+    server.listen(process.env.PORT || 7777, () => {
+      console.log("Server running on port:", process.env.PORT || 7777);
     });
   })
   .catch((err) => {
-    console.error("Database cannot be connected!!");
+    console.error("Database connection failed!!", err);
   });
