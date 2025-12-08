4303e26b6ec55a4714953901f4dffa5b35fb8375
satyammishraa <163456205+satyammishraa@users.noreply.github.com>
2025-12-08 11:34:46 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,13 +13,19 @@ const allowedOrigins = [
   "https://devtinderfrontendnew1.vercel.app",
 ];
 
-
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 
 app.use(express.json());
 app.use(cookieParser());
